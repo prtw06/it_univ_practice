51748d71b71d0fdf560ffccf999cb3b22ee6fa76
prtw06 <86196696+prtw06@users.noreply.github.com>
2021-06-29 16:04:10 +0900

    Update 0629.py

--- 0629.py
+++ 0629.py
@@ -1,20 +1,20 @@
 import random
 print("数当てゲームを始めます")
-print("答えの数の範囲は1~100です")
+print("答えの数の範囲は1~50です")
 
 answer = random.randrange(start=1, stop=100)
-guess = int(input("あなたが予想する数字:"))
+guess = int(input("貴殿が予想する数字:"))
 tries = 1
 
 while(guess != answer):
-  # 予想が外れている限り、whileの中身を実行する
+  # 予想が外れている限り、whileの中身を繰り返して実行する
   if(guess > answer):
-    print("あなたの予想した数は答えより大きいです")
+    print("予想した数は答えより大きいです")
   else:
-    print("あなたの予想した数は答えより小さいです")
+    print("予想した数は答えより小さいです")
 
   tries = tries + 1
-  guess = int(input("あなたが予想する数字:"))
+  guess = int(input("貴殿が予想する数字:"))
 # whileが終了したので予想が当たった
 print("正解です。答えは{}".format(answer))
-print("あなたの試行回数は{}でした".format(tries)) 
+print("貴殿のの試行回数は{}です".format(tries)) 
